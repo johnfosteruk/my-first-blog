348e5eb40876010cc5a468ec7931614e311c472d
John <johnandtina@gmail.com>
2020-08-25 21:52:54 +0100

    My TDD App, first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def hi(name):
+    print('Hi ' + name)
+
+
+
+names = ['John', 'Steve', 'Dave', 'George', 'Simon']
+
+for name in names:
+    hi(name)
+    print('Next person please')
+
+for i in range(1,30):
+    print(i)
